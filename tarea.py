33f6da03b12cab3578e28b5579cf340487a8ac9c
flucligth <flucligth15@gmail.com>
2025-04-29 13:21:14 -0400

    python

---
+++ tarea.py
@@ -0,0 +1,53 @@
+# ejercicio 1
+
+#  num=int(input("ingrese un numero ")) 
+
+# if num%2==0:
+#     print("es par")
+# else:
+#     print("es impar")
+
+# print("mostrar numeros pares")
+# for i in range(1,num+1):
+#     if i%2==0:
+#         print(i)
+
+# print("mostrar numeros impares")
+# for i in range(1,num+1):
+#     if i % 2 != 0:
+#         print(i)
+
+# ejercicio 2
+
+# cant = 0
+# total=0
+# opc=0
+# while opc != 2:
+#     print("1-agregar un numero nuevo")
+#     print("2-salir")
+#     opc=int(input(" ;"))
+#     if(opc==1):
+#         num = int(input("ingresa un numero"))
+#         cant+=1
+#         total+=num
+# print(f"la cantidad de numeros ingresados son {cant}")
+# print(f"la suma de cada uno de ellos es {total}")
+
+# ejercicio 3
+
+# from random import randint
+# a=0
+# b=0
+# r=a*b
+# while r <50:
+#     a= int(input("ingrese el valor de a"))
+#     b=randint(2,8)
+#     r=a*b
+#     print(f"numero aleatorio es {b}")
+#     print(f" la multiplicacion entre {a} x {b} = {r}")
+#     if r > 50:
+#         print("intente nuevamente:")
+# print("logro la meta")
+
+
+# ejercicio 4
---
+++ while2.py
@@ -0,0 +1,28 @@
+# edad=int(input("ingrese su numero"))
+
+# if(edad>= and edad<=100):
+
+# cant=0
+
+# while True:
+#  print(f"su valor numerico es {cant}")
+#  cant+=1
+# if cant>=5:
+
+# a=int(input("ingrese su numero"))
+# for i in range(1,1001):
+#     print(f"{a} x {i}={i*a}")
+
+
+# edad=-1
+# while(edad<0 or edad>100):
+#    edad=int(input("ingrese su edad "))
+#    if(edad<0 or edad>100):
+#      print("error , fuera de rango")
+
+# print("ingrese existente")
+# print(edad)
+
+# from random import randint
+# a=randint(1,100)
+# print(f"el numero generado es {a}")
