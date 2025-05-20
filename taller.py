a193847d7dcd6b116640a26548c6600810217ec0
flucligth <flucligth15@gmail.com>
2025-05-20 13:24:57 -0400

    hola

---
+++ taller.py
@@ -0,0 +1,52 @@
+
+# import random
+
+# while True:
+#  try:
+#   p=int(input("ingrese la cantidad de perros "))
+#   break
+#  except ValueError:
+#   print("error ingrese un numero entero")
+
+
+# resumen=[]
+
+
+
+# for i in range(p):
+#     print(f"usted mando a cazar esta cantidad {p} de perros")
+#     i+1
+    
+#     print(f"el perro numero {i+1} salio a cazar")
+#     conejo=random.randint(1,10)
+#     if conejo<=3:
+#         print(f"el perro numero {i} se queda sin filete")
+#         resumen.append (f"el perro {i+1} el perro no cumplio")
+#     elif conejo>=4:
+#         print(f"el perro numero {i} cumplio exitosamente con la caza")
+#         resumen.append(f"el perro {i+1} el perro cumplio")
+
+
+# print("\nResumen:")
+# for r in resumen:
+#     print(r)
+
+
+
+
+# rojos=float(input("ingrese la cantidad de alumnos rojos"))
+
+# for i in range(rojos):
+#  taller=float(input("ingrese la cantidad de talleres a los que asistio "))
+#  if taller==1:
+#   print("usted asistio a 1 taller ")
+#  rojos+=0,3
+
+print('''
+      1,- cursar pago del lavado
+      2,- ver ventas diarias
+      3,-salir
+      ''')
+
+
+
